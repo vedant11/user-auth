42f0a2003b1010d8d284a102ef80af1d266e90b4
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2020-08-19 12:02:21 +0530

    created react app

--- rest/app/profile/views.py
+++ rest/app/profile/views.py
@@ -12,6 +12,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest.app.user.serializers import UserRegistrationSerializer
 from rest.app.profile.models import UserProfile
+import requests
+import json
 
 
 class UserProfileView(RetrieveAPIView):
@@ -24,24 +26,39 @@ class UserProfileView(RetrieveAPIView):
             user_profile = UserProfile.objects.get(user=request.user)
             status_code = status.HTTP_200_OK
             response = {
-                'success': 'true',
-                'status code': status_code,
-                'message': 'User profile fetched successfully',
-                'data': [{
-                    'first_name': user_profile.first_name,
-                    'last_name': user_profile.last_name,
-                    'phone_number': user_profile.phone_number,
-                    'age': user_profile.age,
-                    'gender': user_profile.gender,
-                    }]
-                }
+                "success": "true",
+                "status code": status_code,
+                "message": "User profile fetched successfully",
+                "data": [
+                    {
+                        "first_name": user_profile.first_name,
+                        "last_name": user_profile.last_name,
+                        "phone_number": user_profile.phone_number,
+                        "age": user_profile.age,
+                        "gender": user_profile.gender,
+                    }
+                ],
+            }
 
+            def get_client_ip(request):
+                x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+                if x_forwarded_for:
+                    ip = x_forwarded_for.split(",")[0]
+                else:
+                    ip = request.META.get("REMOTE_ADDR")
+                return ip
+
+            payload = {"user": user_profile.id, "ip": get_client_ip(request)}
+            r = request.post(
+                "https://encrusxqoan0b.x.pipedream.net/", data=json.dumps(payload)
+            )
+            print(r.text)
         except Exception as e:
             status_code = status.HTTP_400_BAD_REQUEST
             response = {
-                'success': 'false',
-                'status code': status.HTTP_400_BAD_REQUEST,
-                'message': 'User does not exists',
-                'error': str(e)
-                }
+                "success": "false",
+                "status code": status.HTTP_400_BAD_REQUEST,
+                "message": "User does not exists",
+                "error": str(e),
+            }
         return Response(response, status=status_code)
