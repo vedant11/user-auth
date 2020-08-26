1544195beedc6663bbd47720ae0f715fdadcf652
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2020-08-26 13:46:39 +0530

    minor updates

--- jwt_front/src/App.js
+++ jwt_front/src/App.js
@@ -2,49 +2,68 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValues, setInputValues] = useState({
-    form_email: '', form_pass: '', form_first_name: '',
-    form_last_name: '', form_phone_number: '', form_age: '', form_gender: '',
-  });
+	//Single state object
+	const [inputValues, setInputValues] = useState({
+		form_email: '', form_pass: '', form_first_name: '',
+		form_last_name: '', form_phone_number: '', form_age: '', form_gender: '',
+	});
+	//State handler
+	const handleOnChange = event => {
+		const { name, value } = event.target;
+		setInputValues({ ...inputValues, [name]: value });
+	};
+	// POST req handler
+	const sendReq = (e) => {
+		fetch('http://127.0.0.1:8000/api/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				'profile': {
+					'first_name': inputValues.form_first_name,
+					'last_name': inputValues.form_last_name,
+					'phone_number': inputValues.form_phone_number,
+					'age': inputValues.form_age,
+					'gender': inputValues.form_gender,
+				},
+				'email': inputValues.form_email,
+				'password': inputValues.form_pass
+			}),
+		})
+			.then(res => res.json())
+			.then(data => console.log(data))
+		//Prevents redirect
+		e.preventDefault();
+	}
+	return (
+		<>
+			<form onSubmit={sendReq}>
 
-  const handleOnChange = event => {
-    const { name, value } = event.target;
-    setInputValues({ ...inputValues, [name]: value });
-  };
-  const sendReq = (e) => {
-    fetch('http://127.0.0.1:8000/api/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        'profile': {
-          'first_name': inputValues.form_first_name,
-          'last_name': inputValues.form_last_name,
-          'phone_number': inputValues.form_phone_number,
-          'age': inputValues.form_age,
-          'gender': inputValues.form_gender,
-        },
-        'email': inputValues.form_email,
-        'password': inputValues.form_pass
-      }),
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
-    e.preventDefault();
-  }
-  return (
-    <>
-      <form onSubmit={sendReq}>
-        <label>email:</label><input type="email" name="form_email" onChange={handleOnChange} />
-        <label>password:</label><input type="password" name="form_pass" onChange={handleOnChange} />
-        <label>first_name:</label><input type="text" name="form_first_name" onChange={handleOnChange} />
-        <label>last_name:</label><input type="text" name="form_last_name" onChange={handleOnChange} />
-        <label>Phone_number:</label><input type="number" name="form_phone_number" onChange={handleOnChange} />
-        <label>age:</label><input type="number" name="form_age" onChange={handleOnChange} />
-        <label>gender:</label><input type="text" name="form_gender" onChange={handleOnChange} />
-        <input type="submit" value="submit" />
-      </form>
-    </>
-  );
+				<label>email:</label>
+				<input type="email" name="form_email" onChange={handleOnChange} />
+
+				<label>password:</label>
+				<input type="password" name="form_pass" onChange={handleOnChange} />
+
+				<label>first_name:</label>
+				<input type="text" name="form_first_name" onChange={handleOnChange} />
+
+				<label>last_name:</label>
+				<input type="text" name="form_last_name" onChange={handleOnChange} />
+
+				<label>Phone_number:</label>
+				<input type="number" name="form_phone_number" onChange={handleOnChange} />
+
+				<label>age:</label>
+				<input type="number" name="form_age" onChange={handleOnChange} />
+
+				<label>gender:</label>
+				<input type="text" name="form_gender" onChange={handleOnChange} />
+
+				<input type="submit" value="submit" />
+
+			</form>
+		</>
+	);
 }
 
 export default App;
